1175fa6b807a350e29a4a6cf3c12363f973ec16b
Zeeshan Adeen <zeeshan99adeen@gmail.com>
2024-06-12 13:23:15 +0530

    wip-story-thumbnail: initial setup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import {faker} from '@faker-js/faker';
+
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,33 +12,21 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="bg-red-500">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="p-20">
+      <div className="flex space-x-10">
+        {[...Array(20)].map((_, index) => <StoryThumbnail key={index} />)}
+      </div>
     </div>
   );
+}
+
+export const StoryThumbnail = () => {
+  return (
+    <div className="w-[66px] h-[66px] relative flex-[1_0_auto]">
+      <div className="absolute inset-0 w-full h-full rounded-full" style={{
+        background: 'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)'
+      }}></div>
+      <img src={faker.image.url()} alt="" className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-[62px] h-[62px] rounded-full object-cover border-2 border-white" />
+    </div>
+  )
 }
